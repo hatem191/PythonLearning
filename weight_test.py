1510d68c1e251e750c9fb9af912b705c3462d5e1
ORECLent <hmbitbucket@elitemail.org>
2024-09-29 17:54:48 -0500

    Adding weight test

---
+++ weight_test.py
@@ -0,0 +1,6 @@
+number_to_check = int(input("Enter the number you want to check? "))
+
+if number_to_check % 2 == 0:
+    print(f"{number_to_check} is an even number.")
+else:
+    print(f"{number_to_check} is an odd number.")
